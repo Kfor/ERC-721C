1fbe1906e363a7a7d07635d011bb623653feff08
k <k@civiland.io>
2022-04-25 16:57:54 +0800

    update

--- contracts/ComposableFactory.sol
+++ contracts/ComposableFactory.sol
@@ -60,12 +60,11 @@ contract ComposableFactory is IERC721Receiver {
 
     function split(address erc721c, uint256 cId) external {
         require(IERC721(erc721c).ownerOf(cId) == msg.sender, "you must have this ERC721C to split");
-        require(CToQMapping[erc721c][cId].length > 0, "can't split a composed ERC721C");
+        require(CToQMapping[erc721c][cId].length > 0, "can't split a non-exists ERC721C");
         IERC721C(erc721c).burn(msg.sender, cId);
         uint256[] memory qIds = CToQMapping[erc721c][cId];
         address erc721q = CToQAddressMapping[erc721c];
-        for(uint256 i = 0; i < IERC721C(erc721c).getLayerCount() && i < qIds.length; i++) {
-            require(IERC721(erc721q).ownerOf(qIds[i]) == address(this), "no related quark");
+        for(uint256 i = 0; i < qIds.length; i++) {
             IERC721(erc721q).safeTransferFrom(address(this), msg.sender, qIds[i]);
         }
         delete CToQMapping[erc721c][cId];
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,7 @@ import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
+import "hardhat-abi-exporter";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 
@@ -37,6 +38,12 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  abiExporter: {
+    path: "./data/abi",
+    clear: true,
+    flat: true,
+    spacing: 2,
   },
 };
 
